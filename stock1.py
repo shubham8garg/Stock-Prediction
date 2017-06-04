59ab16b3b4603084bca20af5a85f59c2ca595a6b
Shubham <shubham8garg@gmail.com>
2017-06-04 01:36:17 -0400

    Data file and Python code for prediction

---
+++ stock1.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  4 00:26:43 2017
+
+@author: shubh
+"""
+
+import csv
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+
+dates = []
+prices = []
+
+def get_data(filename):
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader)
+        for row in csvFileReader:
+            dates.append(int(row[0].split('-')[0]))
+            prices.append(float(row[1]))
+    return
+
+def predict_prices(dates, prices, x):
+    dates = np.reshape(dates, (len(dates), 1))
+    
+    svr_lin = SVR(kernel = 'linear', C=1e3)
+    svr_poly = SVR(kernel = 'poly', C=1e3, degree=2)
+    svr_rbf = SVR(kernel = 'rbf', C=1e3, gamma = 0.1)
+    svr_lin.fit(dates, prices)
+    svr_poly.fit(dates, prices)
+    svr_rbf.fit(dates, prices)
+    
+    plt.scatter(dates, prices, color='black', label='Data')
+    plt.plot(dates, svr_rbf.predict(dates), color='red', label='RBF model')
+    plt.plot(dates, svr_lin.predict(dates), color='green', label='Linear Model')
+    plt.plot(dates, svr_poly.predict(dates), color='blue', label='Polynomial Model')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Support Vector Regression')
+    plt.legend()
+    plt.show()
+    
+    return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
+
+get_data('C:/Users/shubh/Documents/More Learning/Projects/Stock Prediction/googl.csv')
+
+predicted_price = predict_prices(dates, prices, 31)
+print(predicted_price)
+
+
+
+
+
+        
